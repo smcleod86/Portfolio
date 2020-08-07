757b47c73831cb5ae2f678d6c5e9fb8d99c04b18
smcleod86 <mcleod.scott_86@yahoo.com>
2020-08-07 16:16:30 -0700

    random testing pages for demo

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/navbar'
+
+export default function About() {
+    return (
+        <div>
+            <Navbar />
+            <div>
+                <h1> Hi! My name is Scott.🥔</h1>
+            </div>
+        </div>
+    )
+}
---
+++ pages/blog-posts/post.js
@@ -0,0 +1,5 @@
+export default function Post() {
+    return (
+        <h1>This is a new blog post!!</h1>
+    )
+}
